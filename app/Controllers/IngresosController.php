86ad939df3cf3d0dd3bffbe6c426b9dc61b25d57
Juan Carlos campos <jcampos@supralive.remoto>
2025-02-20 16:47:07 -0500

    Avance para ingresos y egresos

--- app/Controllers/IngresosController.php
+++ app/Controllers/IngresosController.php
@@ -2,12 +2,37 @@
 
 namespace App\Controllers;
 use CodeIgniter\Controller;
+use App\Models\MovimientosModel;
 
 class IngresosController extends Controller {
 
     public function index(){
-        return view('ingresos/index');
+        $movimientosModel = new MovimientosModel();
+        $data['ingresos'] = $movimientosModel->paginate(10);
+        $data['pager']= $movimientosModel->pager;  //Ojo uso pager para mi paginacion basandome en documentacionoficial de codeigniter 4 Jcampos
+        return view('ingresos/index', $data);
     }
+
+    public function guardar(){
+        $movimientosModel = new MovimientosModel();
+        $movimiento = $this->request->getPost('movimientos');
+
+        if(!empty($movimiento)){
+          $movimientosModel->insert(['movimientos'=> $movimiento]);
+          return redirect()->to(base_url('ingresos'))->with('success','Ingreso agregado con exito.');
+        } else {
+            return redirect()->to(base_url('ingresos'))->with('error','El ingreso no fue agregado.');
+        }
+        return redirect()->to(base_url('ingresos'));
+    }
+
+    public function eliminar ($id){
+        $movimientosModel = new MovimientosModel();
+        $movimientosModel->eliminarMovimiento($id);
+        return redirect()->to(base_url('ingresos'));
+    }
+
+   
 }
 
 ?>
---
+++ app/Models/MovimientosModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+Use CodeIgniter\Model;
+
+class MovimientosModel extends Model{
+
+    protected $table = 'movimientos';
+    protected $primaryKey= 'IDmovimiento';
+    protected $allowedFields = ['fechaingreso', 'codigo', 'ubicacion', 'cantidad','observaciones','responsables','tipo'];
+
+    public function agregarMovimiento ($data){
+        return $this->insert($data);
+    }
+
+    public function eliminarMovimiento($id){
+        return $this->delete($id);
+    }
+
+}
